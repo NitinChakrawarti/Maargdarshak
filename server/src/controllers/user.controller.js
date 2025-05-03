9e9336a426eaf95a04ac59ceaabb6a320949360c
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2025-05-03 11:28:21 +0530

    Fix: cookie issue

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -72,8 +72,8 @@ class UserController {
         response.cookie("userToken", token, {
             httpOnly: true,                        
             // secure: process.env.NODE_ENV === "production", 
-            secure: true,                              
-            sameSite: "None",                            
+            secure: false,                              
+            sameSite: "Lax",                            
             maxAge: 30 * 24 * 60 * 60 * 1000,           
         });
 
