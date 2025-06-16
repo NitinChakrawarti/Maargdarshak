4d74d4ba1b183539805e5150c4dfc48ea082ae53
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2025-06-16 13:18:18 +0530

    constants update

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,6 +1,25 @@
 import dotenv from "dotenv"
 
 // ----------------------------------- Initilization of .env file -------------------------------------------
+dotenv.config();
+export const envProvider = {
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
+    EMAIL: process.env.EMAIL,
+    PASSWORD: process.env.PASSWORD,
+    NODE_ENV: process.env.NODE_ENV,
+    FRONT_URL: process.env.FRONT_URL,
+    // Cloudinary Variables
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
+    CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
+    // Clerk Variables
+    CLERK_JWT_PUBLIC_KEY: process.env.CLERK_JWT_PUBLIC_KEY,
+    CLERK_API_KEY: process.env.CLERK_API_KEY,
+    CLERK_FRONTEND_API: process.env.CLERK_FRONTEND_API
+}
+
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:3000",
@@ -23,24 +42,7 @@ export const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization', 'withCredentials']
 };
 
-dotenv.config();
-export const envProvider = {
-    PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI,
-    JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
-    EMAIL: process.env.EMAIL,
-    PASSWORD: process.env.PASSWORD,
-    NODE_ENV: process.env.NODE_ENV,
-    FRONT_URL: process.env.FRONT_URL_,
-    // Cloudinary Variables
-    CLOUD_NAME: process.env.CLOUD_NAME,
-    CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
-    CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
-    // Clerk Variables
-    CLERK_JWT_PUBLIC_KEY: process.env.CLERK_JWT_PUBLIC_KEY,
-    CLERK_API_KEY: process.env.CLERK_API_KEY,
-    CLERK_FRONTEND_API: process.env.CLERK_FRONTEND_API
-}
+
 
 export const baseOptionsforDynamicOptions = {
     discriminatorKey: "optionstype",
