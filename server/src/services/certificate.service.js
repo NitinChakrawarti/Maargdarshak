2e6d006cd56d08461c7e46a12d032f80cb23e2f1
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2025-06-21 17:41:51 +0530

    hotfix (user) : verify certificate

--- server/src/services/certificate.service.js
+++ server/src/services/certificate.service.js
@@ -1,17 +1,15 @@
 import Certificate from "../models/certificate.model.js";
 
 class CertificateService {
-    async verifyCertificate(userId) {
-        const certificate = await Certificate.find({ userId });
+    async verifyCertificate(resourceId) {
+        const certificate = await Certificate.findById(resourceId)
         if (!certificate) {
             return false;
         }
         console.log("Certificate found:", certificate);
 
-        return certificate; 
+        return certificate;
     }
-
-
 }
 
 export default new CertificateService();
